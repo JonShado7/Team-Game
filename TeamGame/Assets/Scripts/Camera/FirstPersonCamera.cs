e221345479396e703be021c2fa261f572ff991d0
JonShado7 <38231163+JonShado7@users.noreply.github.com>
2019-04-14 23:25:01 -0400

    Add Scripts
    Added the Camera and Player Controller Scripts

---
+++ TeamGame/Assets/Scripts/Camera/FirstPersonCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+
+    PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+    }
+}
