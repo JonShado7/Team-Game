6df152312d5d9c6ff475ed3a49b646020bf186c5
JonShado7 <38231163+JonShado7@users.noreply.github.com>
2019-04-26 08:25:04 -0400

    Worked on the Inventory and Crafting System
    I got an Item to add into the inventory by having the player get close and pick it up.

---
+++ TeamGame/Assets/Interact_Object.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact_Object : MonoBehaviour
+{
+
+    //Item to Add to Inventory
+    public ItemDatabase itemToAdd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Inventory.instance.AddItems(itemToAdd);
+            Inventory.instance.UpdateImageUI();
+            Debug.Log("I am here");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ TeamGame/Assets/Scripts/Crafting/Craft.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Craft : MonoBehaviour
+{
+    
+    public CraftingRecipe craft;
+    public ItemDatabase crystals;
+    public ItemDatabase essence;
+    public ItemDatabase database;
+
+    ICraftingContainer iCraft;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CraftItem()
+    {
+        iCraft.ItemCounter(essence);
+        iCraft.ItemCounter(crystals);
+        iCraft.ItemCounter(database);
+        craft.CanCraft(iCraft);
+        craft.Craft(iCraft);
+    }
+
+    
+}
